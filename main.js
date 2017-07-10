4866056cad377af6ab714b29cb9425f77c2dd873
Peter-Fagan <peter_fagan@hotmail.com>
2017-07-10 17:56:36 +1000

    Add boot and assets

--- main.js
+++ main.js
@@ -5,3 +5,4 @@ SideScroller.game = new Phaser.game(746, 420, Phaser.Auto, '');
 SideScroller.game.state.add('Boot', SideScroller.Boot);
 SideScroller.game.state.add('Preload', SideScroller.Preload);
 SideScroller.game.state.add('Game', SideScroller.Game);
+SideScroller.game.state.start('Boot');
